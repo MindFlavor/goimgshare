fbd2efb07dd192df5c4a0fcba42999e68a8c7d6f
Francesco Cogno <francesco.cogno@outlook.com>
2015-10-19 22:18:00 +0200

    completed SharedConfigGenerator

--- sharedConfigGenerator/main.go
+++ sharedConfigGenerator/main.go
@@ -1,52 +1,55 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/mindflavor/goimgshare/folders/physical"
+	"github.com/mindflavor/goimgshare/folders/logical"
 )
 
 var autMails map[string]bool
 var pfs physical.Folders
 var id int
+var rootName string
 
 func main() {
 	root := os.Args[1]
+	outfile := os.Args[2]
 	id = 0
 
-	autMails := make(map[string]bool)
-	for _, item := range os.Args[2:] {
+	autMails = make(map[string]bool)
+	for _, item := range os.Args[3:] {
 		autMails[item] = true
 	}
-
+	
 	pfs = physical.New()
+	
+	_, rootName = filepath.Split(root)
 
-	addFolder(root)
+	addFolder(root, root)
 
-	buf := new(bytes.Buffer)
-
-	err := json.NewEncoder(buf).Encode(pfs)
+	file, err := os.Create(outfile)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 	
-	fmt.Printf("%s", buf.Bytes())
+	pfs.Save(file)
 }
 
-func addFolder(path string) {
-	log.Printf("Adding %s", path)
+func addFolder(root, path string) {
+	// get subpath
+	subpath := path[len(root):]
 
 	// add item
-	f := physical.Folder{Path: path, AuthorizedMails: autMails}
-	f.ID = fmt.Sprintf("%d", id)
+	f := physical.Folder{&logical.Folder{}, path, autMails}
+	f.ID = fmt.Sprintf("%10d", id)
+	f.Name = rootName + subpath
 	id++
-
+	
 	pfs[f.ID] = f
 
 	files, err := ioutil.ReadDir(path)
@@ -56,7 +59,7 @@ func addFolder(path string) {
 
 	for _, file := range files {
 		if file.IsDir() {
-			addFolder(filepath.Join(path, file.Name()))
+			addFolder(root, filepath.Join(path, file.Name()))
 		}
 	}
 }
